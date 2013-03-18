79b3dc66551893ad68c8c2509bb52e35c253e75a
Jonas Didzbalis <jonas@didzbalis.com>
2013-03-17 22:29:46 -0400

    Heroku

--- db/migrate/20130307104348_add_total_return_per_diem_to_project.rb
+++
@@ -1,7 +0,0 @@
-class AddTotalReturnPerDiemToProject < ActiveRecord::Migration
-  def change
-    remove_column :projects, :total_return_per_diem, :integer
-    add_column :projects, :total_return_per_diem, :decimal
-
-  end
-end
